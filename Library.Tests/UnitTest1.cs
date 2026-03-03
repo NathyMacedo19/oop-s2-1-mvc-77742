deda767525a403d8663b00009ce0b4fe032f6aa6
Nathalia Macedo <nathy.fmacedo19@gmail.com>
2026-03-03 12:37:38 +0000

    Add project files.

---
+++ Library.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ThisTestShouldPass()
+        {
+            string name = "John";
+            Assert.Equal("John", name);
+        }
+    }
+}
